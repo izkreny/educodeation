988c65c20fac183f570af4546cdb093a2b4a4674
izkreny <iskren.nemet@gmail.com>
2025-06-03 06:54:27 +0200

    Moved the exercises into the separate file.

---
+++ learn_to_program/01_numbers/exercises.rb
@@ -0,0 +1,11 @@
+# Hours in a year
+puts 365 * 24
+
+# Minutes in a decade
+puts 365 * 24 * 60 * 10
+
+# My age in seconds: 42 years
+puts 365 * 24 * 60 * 60 * 42
+
+# Our dear author’s age: 1_390_000_000 seconds
+puts 1_390_000_000 / (365 * 24 * 60 * 60)
